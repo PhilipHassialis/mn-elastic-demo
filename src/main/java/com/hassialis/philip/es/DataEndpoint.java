724e5aa2a16baddf6196ebb0b5889e2d871f0a75
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-08-13 23:07:17 +0300

    Elastic search - not working because of course it doesn't

--- src/main/java/com/hassialis/philip/es/DataEndpoint.java
+++ src/main/java/com/hassialis/philip/es/DataEndpoint.java
@@ -2,12 +2,19 @@ package com.hassialis.philip.es;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.asyncsearch.AsyncSearchResponse;
+import org.elasticsearch.client.asyncsearch.SubmitAsyncSearchRequest;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,6 +60,33 @@ public class DataEndpoint {
           @Override
           public void onFailure(Exception e) {
             LOG.error("Error while getting document", e);
+            whenDone.completeExceptionally(e);
+          }
+        });
+
+    return whenDone;
+  }
+
+  @Get("/document/async/firstname/{search}")
+  public CompletableFuture<String> byFirstName(@PathVariable("search") String search) {
+    var whenDone = new CompletableFuture<String>();
+
+    final SearchSourceBuilder source = new SearchSourceBuilder()
+        .query(QueryBuilders.matchQuery("first_name", search));
+
+    client.asyncSearch().submitAsync(new SubmitAsyncSearchRequest(source, Constants.INDEX),
+        RequestOptions.DEFAULT,
+        new ActionListener<>() {
+          @Override
+          public void onResponse(final AsyncSearchResponse asyncSearchResponse) {
+            var hits = asyncSearchResponse.getSearchResponse().getHits().getHits();
+            var response = Stream.of(hits).map(SearchHit::getSourceAsString).collect(Collectors.toList());
+            LOG.debug("Response /document/async/firstname/{} => {}", search, response);
+            whenDone.complete(response.toString());
+          }
+
+          @Override
+          public void onFailure(final Exception e) {
             whenDone.completeExceptionally(e);
           }
         });
